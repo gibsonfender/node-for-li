7ca32ee548328b587ce7e37bd6a7a6ed3af8fe52
once <541614829@qq.com>
2017-08-07 16:01:12 +0800

    保存用户聊天数据

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-var express = require('express'),
-    app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server); //引入socket.io模块并绑定到服务器
+var express = require('express');
+var mysql = require('mysql');  
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '123456',
+  database : 'test'
+});
+
+connection.connect();//连接数据库
+
+
+app = express(),
+server = require('http').createServer(app),
+io = require('socket.io').listen(server); //引入socket.io模块并绑定到服务器
 app.use('/', express.static(__dirname + '/www'));
 server.listen(80);
 
@@ -10,9 +22,21 @@ io.on('connection', function(socket) {
     //接收并处理客户端发送的foo事件
     socket.on('foo', function(data) {
         //将消息输出到控制台
-        console.log(data);
+        // console.log(data);
         //发送信息
+        var timestamp=new Date().getTime();
         io.sockets.emit('return', data);
+        
+        //console.log('INSERT INTO node_talk (talk, time,user) VALUES ('+'"'+data+'"'+', '+timestamp+',"user")');
+
+        connection.query('INSERT INTO node_talk (talk, time,user) VALUES ('+'"'+data+'"'+', '+timestamp+',"user")', function (error, results, fields) {
+            if (error) throw error;
+              console.log('The solution is: ', results[0].solution);
+            });//保存聊天数据
+
+
+
 
     })
 });
+
